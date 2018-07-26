3106f7a9bb580ba31398444c5c40adb9d15067cf
Belan Ksenia <ksenia.belan@gmail.com>
2018-07-26 22:08:45 +0300

    Point.class

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,11 @@
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-  }
+    Point point1 = new Point(2, 2);
+    Point point2 = new Point(4, 4);
+    
+    double distance2 = point1.distanceMethod(point2);
 
+    System.out.println("Paccтояние от точки p1 до точки p2 = " + distance2 );
+  }
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,28 @@
+public class Point {
+    private int x;
+
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.getX() - p1.getX()) ^ 2 + (p2.getY() - p1.getY()) ^ 2);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
+
+    public double distanceMethod(Point p2) {
+        return Math.sqrt((p2.getX() - x) ^ 2 + (p2.getY() - y) ^ 2);
+    }
+}
